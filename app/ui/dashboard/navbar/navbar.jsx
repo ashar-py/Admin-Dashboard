26f7381a10c3ab176edfd1e8095512a2f074251a
Bhagyesh Sunil Chaudhari <chaudharibhagyesh11@gmail.com>
2024-01-25 13:13:37 +0530

    navbar updated

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -1,22 +1,55 @@
-"use client"
+// "use client"
+// import { usePathname } from "next/navigation";
+// import styles from "./navbar.module.css"
+// import { MdSearch } from "react-icons/md";
+
+// const Navbar = () => {
+
+//     const pathname = usePathname();
+//     return (
+//         <div className={styles.container}>
+//             <div className={styles.title}>{pathname}</div>
+//             <div className={styles.menu}>
+//                 <div className={styles.search}>
+//                     <MdSearch/>
+//                     <input type="text" placeholder="Search.." className={styles.input}/>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Navbar
+
+"use client";
 import { usePathname } from "next/navigation";
-import styles from "./navbar.module.css"
-import { MdSearch } from "react-icons/md";
+import styles from "./navbar.module.css";
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from "react-icons/md";
 
 const Navbar = () => {
+  const pathname = usePathname();
 
-    const pathname = usePathname();
-    return (
-        <div className={styles.container}>
-            <div className={styles.title}>{pathname}</div>
-            <div className={styles.menu}>
-                <div className={styles.search}>
-                    <MdSearch/>
-                    <input type="text" placeholder="Search.." className={styles.input}/>
-                </div>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>{pathname.split("/").pop()}</div>
+      <div className={styles.menu}>
+        <div className={styles.search}>
+          <MdSearch />
+          <input type="text" placeholder="Search..." className={styles.input} />
         </div>
-    )
-}
+        <div className={styles.icons}>
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
