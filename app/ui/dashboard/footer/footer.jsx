89315d416982604a53274185e48046daac6c9b6b
rash <ashar.shakil04@gmail.com>
2024-01-29 15:10:37 +0530

    Minor UI changes

--- app/ui/dashboard/footer/footer.jsx
+++ app/ui/dashboard/footer/footer.jsx
@@ -3,7 +3,7 @@ import styles from './footer.module.css';
 const Footer = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.logo}>Trini AI</div>
+            <div className={styles.logo}> Trini AI </div>
             <div className={styles.address}> © All rights reserved</div>
         </div>
     )
