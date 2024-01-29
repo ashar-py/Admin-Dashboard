5a9b927eb184f3efc6db4014616288b49728e3b8
Bhagyesh Sunil Chaudhari <chaudharibhagyesh11@gmail.com>
2024-01-29 14:35:44 +0530

    logout added

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -95,7 +95,13 @@ const Sidebar = () => {
     return (
         <div className={styles.container}>
             <div className={styles.user}>
-                <Image className={styles.userImage} src="/noavatar.png" width="50" height="50" />
+            <Image
+          className={styles.userImage}
+          src={user.img || "/noavatar.png"}
+          alt=""
+          width="50"
+          height="50"
+        />
                 <div className={styles.userDetail}>
                     <span className={styles.username}>John Doe</span>
                     <span className={styles.userTitle}>Adminitstrator</span>
@@ -111,8 +117,17 @@ const Sidebar = () => {
                     </li>
             ))}
             </ul>
-            <button className={styles.logout}>
-                <MdLogout />Logout</button>
+            <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button className={styles.logout}>
+          <MdLogout />
+          Logout
+        </button>
+      </form>
             </div>
     )
 }
