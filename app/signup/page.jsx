86b3867a93bfa4a9e6c3e4e2ec2a5ad63e09e1e8
Bhagyesh Sunil Chaudhari <chaudharibhagyesh11@gmail.com>
2024-02-13 11:53:57 +0530

    sigup added

---
+++ app/signup/page.jsx
@@ -0,0 +1,12 @@
+import styles from "@/app/ui/login/login.module.css";
+
+
+const RegisterPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Signup page</h1>
+    </div>
+  );
+};
+
+export default RegisterPage;
