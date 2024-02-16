e2d25fb785d2b68bae24a94f7b887b5d0ff8e777
Bhagyesh Sunil Chaudhari <chaudharibhagyesh11@gmail.com>
2024-02-16 15:02:49 +0530

    training page added

--- components/agents/TrainingMaterial.jsx
+++ components/agents/TrainingMaterial.jsx
@@ -1,13 +1,68 @@
 "use client";
-import React from 'react'
+// TrainingMaterial.js
+import React, { useState } from 'react';
 import styles from "@/app/ui/dashboard/trainingmaterial/training.module.css";
 
 function TrainingMaterial() {
+  const [activeTab, setActiveTab] = useState('fileUpload');
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+    switch (tab) {
+      case 'fileUpload':
+        break;
+      case 'websiteURL':
+        break;
+      case 'sitemap':
+        break;
+      case 'text':
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className={styles.trainingMaterialContainer}>
-      <div className={styles.uploadContainer}>
-    
-        <input type="file" />
+      <div className={styles.topContainer}>
+        <div className={styles.tabContainer}>
+          <div
+            className={`${styles.tab} ${activeTab === 'fileUpload' ? styles.activeTab : ''}`}
+            onClick={() => handleTabClick('fileUpload')}
+          >
+            File Upload
+          </div>
+          <div
+            className={`${styles.tab} ${activeTab === 'websiteURL' ? styles.activeTab : ''}`}
+            onClick={() => handleTabClick('websiteURL')}
+          >
+            Website URL
+          </div>
+          <div
+            className={`${styles.tab} ${activeTab === 'sitemap' ? styles.activeTab : ''}`}
+            onClick={() => handleTabClick('sitemap')}
+          >
+            Sitemap
+          </div>
+          <div
+            className={`${styles.tab} ${activeTab === 'text' ? styles.activeTab : ''}`}
+            onClick={() => handleTabClick('text')}
+          >
+            Text
+          </div>
+        </div>
+        {activeTab === 'fileUpload' && (
+          <input type="file" />
+        )}
+        {activeTab === 'websiteURL' && (
+          <input type="text" placeholder="Enter website URL" />
+        )}
+        {activeTab === 'sitemap' && (
+          <input type="text" placeholder="Enter sitemap URL" />
+        )}
+        {activeTab === 'text' && (
+          <textarea placeholder="Enter text"></textarea>
+        )}
       </div>
       <div className={styles.tableContainer}>
        
@@ -22,68 +77,76 @@ function TrainingMaterial() {
               <th>Last Trained</th>
               <th>Re-train</th>
               <th>Delete</th>
-              
-              
-             
             </tr>
           </thead>
           <tbody>
+            <tr>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+            </tr>
+            <tr>
+              <td>Sample 2</td>
+              <td>Sample 2</td>
+              <td>Sample 2</td>
+              <td>Sample 2</td>
+              <td>Sample 2</td>
+              <td>Sample 2</td>
+              <td>Sample 2</td>
+              <td>Sample 2</td>
+            </tr>
+            <tr>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+            </tr>
+            <tr>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+            </tr>
+            <tr>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+            </tr>
+            <tr>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+              <td>Sample 1</td>
+            </tr>
             
-            <tr>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              
-            </tr>
-            <tr>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              
-            </tr>
-            <tr>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              
-            </tr>
-            <tr>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              <td>Sample 1</td>
-              
-            </tr>
-
-            
+           
           </tbody>
         </table>
-        
-        <div className={styles.filterOptions}>
-         
-        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default TrainingMaterial
+export default TrainingMaterial;
