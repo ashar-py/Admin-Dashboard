a6e073250f302a46b52863f8a9eedbe326b83bce
Bhagyesh Sunil Chaudhari <chaudharibhagyesh11@gmail.com>
2024-02-17 23:41:42 +0530

    page updated

--- components/agents/FAQS.jsx
+++ components/agents/FAQS.jsx
@@ -24,24 +24,19 @@ const FAQS = () => {
         <h4>Helpful Tips</h4>
         
         <li>
-          Set manner rules for the agent (You will be friendly, warm and
-          understanding. You will be mildly persuasive but never come off as
-          annoying)
+        Detailed information about your company (Name, Location, Testimonials, Expertise, USP)
+
         </li>
         <li>
-          Set conversation leading guidelines (You will lead the conversation,
-          making sure the user answers one question clearly before moving to the
-          next. You will stick to context. Any discussions outside context, you
-          will steer back to the conversation)
+        Detailed information about your products (Sizes, Brands, Cost)
+
         </li>
         <li>
-          Set method of speaking rules for the agent (You will never speak more
-          than 1-2 sentences. Keep answers short and crisp)
+        Detailed information about the service (Time to deliver, areas you deliver in)
+
         </li>
         <li>
-          Set company guidelines for the agent (You will never speak about
-          competitors like Loafers Inc. You will never disclose anything about
-          Sneakers Inc outside what is in your prompt)
+        Detailed information about discounts, reviews, or anything else you feel your agent should be equipped to answer.
         </li>
         
       </div>
