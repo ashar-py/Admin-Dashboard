914ac87d7d96565c1665da425d77bda8d7724e3a
Bhagyesh Sunil Chaudhari <chaudharibhagyesh11@gmail.com>
2024-03-01 23:23:37 +0530

    DD

--- components/stats/activeusers.jsx
+++ components/stats/activeusers.jsx
@@ -1,96 +1,3 @@
-// "use client";
-// import React, { useState, useEffect } from "react";
-// import { fetchActiveUsers } from "@/app/api/activeusers";
-// import UserInfo from "@/components/stats/userinfo";
-// import styles from "@/app/ui/dashboard/stats/table/list.module.css";
-
-
-// const ActiveUsers = () => {
-//   const [activeUsers, setActiveUsers] = useState([]);
-//   const [error, setError] = useState(null);
-//   const [selectedPhoneNo, setSelectedPhoneNo] = useState(null); 
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const dataObject = await fetchActiveUsers();
-//         const dataArray = Object.values(dataObject);
-//         setActiveUsers(dataArray);
-//         setError(null);
-//       } catch (error) {
-//         console.error("Error fetching active users:", error);
-//         setError(error.message || "An error occurred while fetching data.");
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div className={styles.container}>
-//       <h4 className={styles.title}>STATS</h4>
-//       {error ? (
-//         <p>Error: {error}</p>
-//       ) : (
-//         <>
-//           <table className={styles.mainTable}>
-//             <thead>
-//               <tr>
-//                 <th>BUSINESS NAME</th>
-//                 <th>PHONE NUMBER</th>
-               
-//                 <th>SUMMARY</th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               {activeUsers.map((userData) => {
-//                 return Object.entries(userData).map(([phone_no, userData]) => {
-//                   if (
-//                     userData.name &&
-//                     userData.phone_no &&
-                   
-//                     userData.assistant_id
-//                   ) {
-//                     return (
-//                       <tr key={userData.assistant_id}>
-//                         <td>{userData.name}</td>
-//                         <td>{userData.phone_no}</td>
-                        
-//                         <td>
-//                           <button 
-//                             onClick={() => setSelectedPhoneNo(userData.phone_no)}
-//                             className={`${styles.button} ${styles.view}`}
-//                           >
-//                             View
-//                           </button>
-//                         </td>
-//                       </tr>
-//                     );
-//                   } else {
-//                     console.warn("Incomplete data:", userData);
-//                     return null;
-//                   }
-//                 });
-//               })}
-//             </tbody>
-//           </table>
-//           {selectedPhoneNo && (
-//             <div  className={styles.popupOverlay}>
-//             <div className={styles.popup}>
-//               <button className={styles.button} onClick={() => setSelectedPhoneNo(null)}>Close</button>
-//               <UserInfo phoneNumber={selectedPhoneNo} /> 
-              
-//             </div>
-//             </div>
-//           )}
-//         </>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default ActiveUsers;
-
 
 "use client";
 import React, { useState, useEffect } from "react";
@@ -160,9 +67,9 @@ const ActiveUsers = () => {
           <table className={styles.mainTable}>
             <thead>
               <tr>
-                <th>BUSINESS NAME</th>
+                <th>USER NAME</th>
                 <th>PHONE NUMBER</th>
-                <th>SUMMARY</th>
+                <th> CHAT SUMMARY</th>
               </tr>
             </thead>
             <tbody>
@@ -202,7 +109,7 @@ const ActiveUsers = () => {
             <div className={styles.popupOverlay}>
               <div className={styles.popup}>
                 <button className={styles.button} onClick={() => setSelectedPhoneNo(null)}>Close</button>
-                {/* Pass selectedPhoneNo and dropdownValue to UserInfo component */}
+                
                 <UserInfo phoneNumber={selectedPhoneNo} prefix={dropdownValue} />
               </div>
             </div>
