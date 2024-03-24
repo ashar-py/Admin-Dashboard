e703a6eb41a6404c60d00854902692547703e6c7
Bhagyesh Sunil Chaudhari <chaudharibhagyesh11@gmail.com>
2024-03-24 12:56:07 +0530

    done

--- components/stats/activeusers.jsx
+++ components/stats/activeusers.jsx
@@ -11,7 +11,7 @@ const ActiveUsers = () => {
   const [activeUsers, setActiveUsers] = useState([]);
   const [error, setError] = useState(null);
   const [selectedPhoneNo, setSelectedPhoneNo] = useState(null);
-  const [dropdownValue, setDropdownValue] = useState("#"); // Default valueee
+  const [dropdownValue, setDropdownValue] = useState("#"); // Default valueeee
 
   useEffect(() => {
     const fetchData = async () => {
